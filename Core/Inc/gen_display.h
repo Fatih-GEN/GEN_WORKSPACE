8ff0d601fbfcd04f2821839275602b644d975517
genemekgokhan <97452113+genemekgokhan@users.noreply.github.com>
2022-01-20 11:47:51 +0300

    added gen_display.c and .header file

---
+++ Core/Inc/gen_display.h
@@ -0,0 +1,13 @@
+/*
+ * gen_display.h
+ *
+ *  Created on: Jan 20, 2022
+ *      Author: gokhan.tekinok
+ */
+
+#ifndef INC_GEN_DISPLAY_H_
+#define INC_GEN_DISPLAY_H_
+
+
+
+#endif /* INC_GEN_DISPLAY_H_ */
